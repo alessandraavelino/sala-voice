0917f7a9edc38e8c44127bdedfe11692dd369073
Alessandra Avelino <alessavelino123@gmail.com>
2024-02-16 13:40:04 -0300

    Initial commit

---
+++ public/index.js
@@ -0,0 +1,129 @@
+const mapImage = new Image();
+mapImage.src = "/snowy-sheet.png";
+
+const santaImage = new Image();
+santaImage.src = "/boy.png";
+
+const canvasEl = document.getElementById("canvas");
+canvasEl.width = window.innerWidth;
+canvasEl.height = window.innerHeight;
+const canvas = canvasEl.getContext("2d");
+
+const socket = io();
+
+let groundMap = [[]];
+let decalMap = [[]];
+let players = [];
+
+const TILE_SIZE = 32;
+
+socket.on("connect", () => {
+  console.log("connected");
+});
+
+socket.on("map", (loadedMap) => {
+  groundMap = loadedMap.ground;
+  decalMap = loadedMap.decal;
+});
+
+socket.on("players", (serverPlayers) => {
+  players = serverPlayers;
+});
+
+
+const inputs = {
+  up: false,
+  down: false,
+  left: false,
+  right: false,
+};
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "w") {
+    inputs["up"] = true;
+  } else if (e.key === "s") {
+    inputs["down"] = true;
+  } else if (e.key === "d") {
+    inputs["right"] = true;
+  } else if (e.key === "a") {
+    inputs["left"] = true;
+  }
+  socket.emit("inputs", inputs);
+});
+
+window.addEventListener("keyup", (e) => {
+  if (e.key === "w") {
+    inputs["up"] = false;
+  } else if (e.key === "s") {
+    inputs["down"] = false;
+  } else if (e.key === "d") {
+    inputs["right"] = false;
+  } else if (e.key === "a") {
+    inputs["left"] = false;
+  }
+  socket.emit("inputs", inputs);
+});
+
+
+function loop() {
+  canvas.clearRect(0, 0, canvasEl.width, canvasEl.height);
+
+  const myPlayer = players.find((player) => player.id === socket.id);
+  let cameraX = 0;
+  let cameraY = 0;
+  if (myPlayer) {
+    cameraX = parseInt(myPlayer.x - canvasEl.width / 2);
+    cameraY = parseInt(myPlayer.y - canvasEl.height / 2);
+  }
+
+  const TILES_IN_ROW = 8;
+
+  // ground
+  for (let row = 0; row < groundMap.length; row++) {
+    for (let col = 0; col < groundMap[0].length; col++) {
+      let { id } = groundMap[row][col];
+      const imageRow = parseInt(id / TILES_IN_ROW);
+      const imageCol = id % TILES_IN_ROW;
+      canvas.drawImage(
+        mapImage,
+        imageCol * TILE_SIZE,
+        imageRow * TILE_SIZE,
+        TILE_SIZE,
+        TILE_SIZE,
+        col * TILE_SIZE - cameraX,
+        row * TILE_SIZE - cameraY,
+        TILE_SIZE,
+        TILE_SIZE
+      );
+    }
+  }
+
+  // decals
+  for (let row = 0; row < decalMap.length; row++) {
+    for (let col = 0; col < decalMap[0].length; col++) {
+      let { id } = decalMap[row][col] ?? { id: undefined };
+      const imageRow = parseInt(id / TILES_IN_ROW);
+      const imageCol = id % TILES_IN_ROW;
+
+      canvas.drawImage(
+        mapImage,
+        imageCol * TILE_SIZE,
+        imageRow * TILE_SIZE,
+        TILE_SIZE,
+        TILE_SIZE,
+        col * TILE_SIZE - cameraX,
+        row * TILE_SIZE - cameraY,
+        TILE_SIZE,
+        TILE_SIZE
+      );
+    }
+  }
+
+  for (const player of players) {
+    canvas.drawImage(santaImage, player.x - cameraX, player.y - cameraY);
+  }
+
+  window.requestAnimationFrame(loop);
+}
+
+window.requestAnimationFrame(loop);
---
+++ src/index.js
@@ -0,0 +1,138 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer);
+
+const PORT = process.env.PORT || 5000;
+
+const loadMap = require("./mapLoader");
+
+const SPEED = 5;
+const TICK_RATE = 30;
+const PLAYER_SIZE = 32;
+const TILE_SIZE = 32;
+
+let players = [];
+const inputsMap = {};
+let ground2D, decal2D;
+
+function isColliding(rect1, rect2) {
+  return (
+    rect1.x < rect2.x + rect2.w &&
+    rect1.x + rect1.w > rect2.x &&
+    rect1.y < rect2.y + rect2.h &&
+    rect1.h + rect1.y > rect2.y
+  );
+}
+
+function isCollidingWithMap(player) {
+  for (let row = 0; row < decal2D.length; row++) {
+    for (let col = 0; col < decal2D[0].length; col++) {
+      const tile = decal2D[row][col];
+
+      if (
+        tile &&
+        isColliding(
+          {
+            x: player.x,
+            y: player.y,
+            w: 32,
+            h: 32,
+          },
+          {
+            x: col * TILE_SIZE,
+            y: row * TILE_SIZE,
+            w: TILE_SIZE,
+            h: TILE_SIZE,
+          }
+        )
+      ) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function tick(delta) {
+  for (const player of players) {
+    const inputs = inputsMap[player.id];
+    const previousY = player.y;
+    const previousX = player.x;
+
+    if (inputs.up) {
+      player.y -= SPEED;
+    } else if (inputs.down) {
+      player.y += SPEED;
+    }
+
+    if (isCollidingWithMap(player)) {
+      player.y = previousY;
+    }
+
+    if (inputs.left) {
+      player.x -= SPEED;
+    } else if (inputs.right) {
+      player.x += SPEED;
+    }
+
+    if (isCollidingWithMap(player)) {
+      player.x = previousX;
+    }
+  }
+ 
+
+  io.emit("players", players);
+}
+
+async function main() {
+  ({ ground2D, decal2D } = await loadMap());
+
+  io.on("connect", (socket) => {
+    console.log("user connected", socket.id);
+
+    inputsMap[socket.id] = {
+      up: false,
+      down: false,
+      left: false,
+      right: false,
+    };
+
+    players.push({
+      id: socket.id,
+      x: 800,
+      y: 800,
+    });
+
+    socket.emit("map", {
+      ground: ground2D,
+      decal: decal2D,
+    });
+
+    socket.on("inputs", (inputs) => {
+      inputsMap[socket.id] = inputs;
+    });
+
+    socket.on("disconnect", () => {
+      players = players.filter((player) => player.id !== socket.id);
+    });
+  });
+
+  app.use(express.static("public"));
+
+  httpServer.listen(PORT);
+
+  let lastUpdate = Date.now();
+  setInterval(() => {
+    const now = Date.now();
+    const delta = now - lastUpdate;
+    tick(delta);
+    lastUpdate = now;
+  }, 1000 / TICK_RATE);
+}
+
+main();
